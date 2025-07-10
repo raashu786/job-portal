e1501e0cf2c205c06141c09fe094d05a4d22a92f
Md rashid Ali <raashid.ali0@gmail.com>
2025-07-10 11:20:53 +0530

    Update AxiosInterceptor.tsx

--- src/Interceptor/AxiosInterceptor.tsx
+++ src/Interceptor/AxiosInterceptor.tsx
@@ -1,7 +1,7 @@
 import axios, { InternalAxiosRequestConfig } from "axios";
 const axiosInstance = axios.create({
-   baseURL: 'http://localhost:8080',
-    //baseURL: 'https://job-portal-backend-smwt.onrender.com',
+  // baseURL: 'http://localhost:8080',
+    baseURL: 'https://job-portal-backend-smwt.onrender.com',
   });
 
   axiosInstance.interceptors.request.use(
